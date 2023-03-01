67968b258f8c745f67651b6b28934d87d11e4078
stanleyjoe33 <stanlex33@gmail.com>
2023-03-01 07:18:22 +0100

    defined models.py

--- BlogPost/BlogPost/settings.py
+++ BlogPost/BlogPost/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "Blog",
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+01:00'
 
 USE_I18N = True
 
