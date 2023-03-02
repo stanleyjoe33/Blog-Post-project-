c6b81c48b5bc266e6fcef4e7608f54dbaeefd80f
stanleyjoe33 <stanlex33@gmail.com>
2023-03-02 23:22:58 +0100

    Restarted project because of typo in project name

--- MindfulMatters/MindfulMatters/asgi.py
+++ MindfulMatters/MindfulMatters/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MinfulMatters project.
+ASGI config for MindfulMatters project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MinfulMatters.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MindfulMatters.settings')
 
 application = get_asgi_application()
--- MindfulMatters/MindfulMatters/wsgi.py
+++ MindfulMatters/MindfulMatters/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MinfulMatters project.
+WSGI config for MindfulMatters project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MinfulMatters.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MindfulMatters.settings')
 
 application = get_wsgi_application()
