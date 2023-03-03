45b1621e352ae2da760a53d2ed6e05e900d32b26
stanleyjoe33 <stanlex33@gmail.com>
2023-03-03 01:01:51 +0100

    added template context to views

--- MindfulMatters/blog/views.py
+++ MindfulMatters/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 def homepage(request):
-    return render(request, 'homepage.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context = {'posts': posts}
+    return render(request, 'homepage.html', )
