24d922c6ed3094a661684f6e7813a2a0243eeb56
stanleyjoe33 <stanlex33@gmail.com>
2023-03-04 23:12:12 +0100

    fixed filter added by boiler code in views

--- MindfulMatters/blog/views.py
+++ MindfulMatters/blog/views.py
@@ -3,6 +3,6 @@ from .models import Post
 from django.utils import timezone
 # Create your views here.
 def homepage(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().order_by('published_date')
     context = {'posts': posts}
     return render(request, 'homepage.html', context)
