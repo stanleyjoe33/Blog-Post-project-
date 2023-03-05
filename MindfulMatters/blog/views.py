e82dc9894d7a41bbcb0d26c0b019d1b38a9d85a8
stanleyjoe33 <stanlex33@gmail.com>
2023-03-05 20:25:57 +0100

    updated view

--- MindfulMatters/blog/views.py
+++ MindfulMatters/blog/views.py
@@ -4,5 +4,7 @@ from django.utils import timezone
 # Create your views here.
 def homepage(request):
     posts = Post.objects.all().order_by('published_date')
+    #the step below makes all the post objects created above 
+    # a value to the key "posts" in context, so that they can be accessed in the view.
     context = {'posts': posts}
     return render(request, 'homepage.html', context)
